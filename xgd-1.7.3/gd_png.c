8ea35b1c4ddcfc8ccca8603232539901c7874ce6
Caolán McNamara <caolanm@redhat.com>
2018-08-08 13:06:20 +0100

    png_set_dither->png_set_quantize

--- xgd-1.7.3/gd_png.c
+++ xgd-1.7.3/gd_png.c
@@ -265,7 +265,7 @@ gdImagePtr gdImageCreateFromPngCtx(gdIOCtx *infile)
                 png_color_16p histogram;
 
                 png_get_hIST(png_ptr, info_ptr, &histogram);
-                png_set_dither(png_ptr, palette, num_palette,
+                png_set_quantize(png_ptr, palette, num_palette,
                   max_screen_colors, histogram, 1);
             } else
 #endif
@@ -291,7 +291,7 @@ gdImagePtr gdImageCreateFromPngCtx(gdIOCtx *infile)
                 palette[2].red = palette[2].green = palette[2].blue = 128;
                 palette[3].red = palette[3].green = palette[3].blue = 64;
                 /* final argument (full_dither) *must* be 1: */
-                png_set_dither(png_ptr, palette, 256, 256, NULL, 1);
+                png_set_quantize(png_ptr, palette, 256, 256, NULL, 1);
 #else
                 /* allocate a 6x6x6 color cube, starting at index 0 or 1 */
                 idx = (transparent < 0)? 0 : 1;
@@ -305,7 +305,7 @@ gdImagePtr gdImageCreateFromPngCtx(gdIOCtx *infile)
                         }
                     }
                 }
-                png_set_dither(png_ptr, palette, idx, idx, NULL, 1);
+                png_set_quantize(png_ptr, palette, idx, idx, NULL, 1);
 #endif
             }
             break;
